0631d4f042e8686fe52a51bedf4d4a77eac352ce
Maulik Fojdar <mfojdar@cisco.com>
2016-10-20 11:35:08 -0700

    Made the code as per standard documentation and added comments to class definition

--- Movie Trailer/entertainment_center.py
+++ Movie Trailer/entertainment_center.py
@@ -2,42 +2,57 @@ import fresh_tomatoes
 import media
 
 # Initialize the project with movie data
-batman_begins = media.Movie("Batman Begins", "First Movie of Batman: Dark Knight Trilogy",
-                            "https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg",
-                            "https://www.youtube.com/watch?v=neY2xVmOfUM")
+batman_begins = media.Movie(
+    "Batman Begins",
+    "First Movie of Batman: Dark Knight Trilogy",
+    "https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg",
+    "https://www.youtube.com/watch?v=neY2xVmOfUM")
 
-dark_knight = media.Movie("The Dark Knight", "Second Movie of Batman: Dark Knight Trilogy",
-                          "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
-                          "https://www.youtube.com/watch?v=yrJL5JxEYIw")
+dark_knight = media.Movie(
+    "The Dark Knight",
+    "Second Movie of Batman: Dark Knight Trilogy",
+    "https://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
+    "https://www.youtube.com/watch?v=yrJL5JxEYIw")
 
-dark_knight_rises = media.Movie("The Dark Knight Rises", "Third Movie of Batman: Dark Knight Trilogy",
-                                "https://upload.wikimedia.org/wikipedia/en/8/83/Dark_knight_rises_poster.jpg",
-                                "https://www.youtube.com/watch?v=J9DlV9qwtF0")
+dark_knight_rises = media.Movie(
+    "The Dark Knight Rises",
+    "Third Movie of Batman: Dark Knight Trilogy",
+    "https://upload.wikimedia.org/wikipedia/en/8/83/Dark_knight_rises_poster.jpg",
+    "https://www.youtube.com/watch?v=J9DlV9qwtF0")
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that become real",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys that become real",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar", "An epic science fiction Movie which showed alien life on a planet called pandora",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+avatar = media.Movie(
+    "Avatar",
+    "An epic science fiction Movie which showed alien life on pandora",
+    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-avengers = media.Movie("Avengers", "A superhero movie based on Marvels comics",
-                       "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
-                       "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+avengers = media.Movie(
+    "Avengers",
+    "A superhero movie based on Marvels comics",
+    "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
+    "https://www.youtube.com/watch?v=eOrNdBpGMv8")
 
-the_proposal = media.Movie("The Proposal", "Here comes the bride",
-                           "https://upload.wikimedia.org/wikipedia/en/0/02/The_Proposal.jpg",
-                           "https://www.youtube.com/watch?v=RFL8b1p1ELY")
+the_proposal = media.Movie(
+    "The Proposal",
+    "Here comes the bride",
+    "https://upload.wikimedia.org/wikipedia/en/0/02/The_Proposal.jpg",
+    "https://www.youtube.com/watch?v=RFL8b1p1ELY")
 
-titanic = media.Movie("Titanic", "Story of Love",
-                      "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",
-                      "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+titanic = media.Movie(
+    "Titanic",
+    "Story of Love",
+    "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",
+    "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
 
-#Creating the array of movie data
-movies = [batman_begins, dark_knight, dark_knight_rises, toy_story, avatar, avengers, the_proposal, titanic]
-#Creating the web page
+# Creating the array of movie data
+movies = [batman_begins, dark_knight, dark_knight_rises, toy_story, avatar,
+          avengers, the_proposal, titanic]
+# Creating the web page
 fresh_tomatoes.create_movie_tiles_content(movies)
 fresh_tomatoes.open_movies_page(movies)
-
-    
--- Movie Trailer/media.py
+++ Movie Trailer/media.py
@@ -1,6 +1,11 @@
 import webbrowser
 
 class Movie():
+    """
+        This Class defines the movie and its attributes.
+         It contains attributes like movie title, movie story, poster image and youtube trailer
+    """
+
     def __init__(self, movie_title, movie_story, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_story
